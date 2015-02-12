730f029d071b4ed6f177ec228f57db97c02d4b60
jason <jason>
2015-02-12 08:37:19 -0500

    Add register, login, logout link to index.php

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
 		</title>
 	</head>
 	<body>
-            <h2>Hello world brah!</h2>
+			<?php session_start(); ?>
+            
+            <?php
+	            if(isset($_SESSION['sess_username'])){
+	            	echo "<h2>Welcome ".$_SESSION['sess_username'].'!</h2>';
+				    echo "you logged in as ", $_SESSION['sess_username'];
+				    echo "<br/><a href='logout.php'>logout</a>";
+				}
+				else{
+					echo 'Welcome!';
+					echo "<br/><a href='logintest.php'>login</a>";
+					echo "<br/><a href='register.php'>sign up</a>";
+				}
+			?>
 	</body>
 </html>
