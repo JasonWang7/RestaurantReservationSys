68039819a766b46e404837ffca48770a7ef7b514
rhysinator19 <the_rock_2462@hotmail.com>
2015-02-12 01:16:32 -0500

    Edited footer
    Signed-off-by: rhysinator19 <the_rock_2462@hotmail.com>

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,72 @@
 <?php
-echo "<p>Copyright &copy;" . date("Y") . " Centaur Logic</p>";
+/******************************************************************************************
+* File name: footer.php
+* Purpose: Contains the footer for the bottom of every page in the website
+* Author: Rhys Hall
+* Group Name: Centaur Logic
+* Created: February 11, 2015
+******************************************************************************************/
 ?>
+
+<html>
+<body>
+
+<!-- "About Us" link -->
+<p>
+<a href="" style="font-size: 15px; text-decoration: underline">About Us</a> 
+</p>
+
+<!-- "Dining Rewards" link -->
+<p>
+<a href="" style="font-size: 15px; text-decoration: underline">Dining Rewards</a> 
+</p>
+
+<!-- "Top Restaurants" link -->
+<p>
+<a href="" style="font-size: 15px; text-decoration: underline">Top Restaurants</a> 
+</p>
+
+<!-- "Invite Friends" link -->
+<p>
+<a href="" style="font-size: 15px; text-decoration: underline">Invite Friends</a> 
+</p>
+
+<!-- "Events/Promotions" link -->
+<p>
+<a href="" style="font-size: 15px; text-decoration: underline">Events/Promotions</a> 
+</p>
+
+<br>
+
+<!-- Style sheet obtained from http://bootsnipp.com/snippets/featured -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+<div class="container">
+<h4>Share</h4>
+	<hr>
+                <a href="https://www.facebook.com/bootsnipp"><i id="social" class="fa fa-facebook-square fa-2x social-fb"></i></a> 
+				
+				<!-- space social network icons apart -->
+				&nbsp;
+				
+	            <a href="https://twitter.com/bootsnipp"><i id="social" class="fa fa-twitter-square fa-2x social-tw"></i></a>
+				
+				&nbsp;
+				
+	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social" class="fa fa-google-plus-square fa-2x social-gp"></i></a>
+				
+				&nbsp;
+				
+	            <a href="mailto:bootsnipp@gmail.com"><i id="social" class="fa fa-envelope-square fa-2x social-em"></i></a>
+
+    <hr>
+</div>
+
+</body>
+</html>
+
+<br><br><br>
+
+<p style="font-family:courier"><i>Copyright @2015. Centaur Logic</i></p>
+
+<br><br>
