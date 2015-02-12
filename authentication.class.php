a5b1b6a083827030c11e36da102b5943053f1a71
jason <jason>
2015-02-12 08:20:26 -0500

    Add authentication class

---
+++ authentication.class.php
@@ -0,0 +1,73 @@
+<?php
+/*****author: Jason Wang*******/
+require_once('database.class.php');
+class authentication{
+	private $useremail;
+	private $password;
+	private $role="regular";
+	private $status="baned";
+	private $username="";
+	private $userid ='';
+
+
+	/**
+     * Sanitize data
+     * @param String the data to be sanitized
+     * @return String the sanitized data
+     */
+	function sanitize($data){
+
+		return real_escape_string($data);
+	}
+
+	/**
+     * login verification 
+     * @param email
+     * @param password
+     * @return none
+     */
+	function login($email, $passwordparam){
+		ob_start();  //tempory turn on buffer
+		session_start();
+		$useremail = $email;
+		$password = $passwordparam;
+		/*if(!isset($this->post['email']) && $this->post['email']==''&&!isset($this->post['password']))*/
+		$isAuthenticated = false;
+		$isAutenticated = mysqldatabaserrs::verifyUser($email,authentication::encryptPass($password));
+		if($isAutenticated != true){  //user not fould or authenticated, redirect
+			
+			header('Location: logintest.php');
+		}
+		else{
+			/***verified,retrive user info, generate session id, save value***/
+			/*******SELECT USER DATA FROM DB***/
+			$userInfo = mysqldatabaserrs::getBasicUserInfo($email);
+			/****check if user status*****/
+			if($userInfo->getStatus()!='baned'){
+				session_regenerate_id();
+				$_SESSION['sess_user_id'] = $userInfo->getUserId();
+				$_SESSION['sess_username'] = $userInfo->getUserName();
+				$_SESSION['sess_useremail'] = $email;
+				$userInfo->getUserName();
+				session_write_close();
+				header('Location: index.php');
+			}
+			else{
+				header('Location: suspended.php');
+			}
+			
+		}		
+	}
+
+
+	/**
+     * encrypt password
+     * @param password
+     * @return encrypted password
+     */
+	function encryptPass($pass){
+		return $encrptedPass = hash('sha256',$pass);
+	}
+
+}
+?>
