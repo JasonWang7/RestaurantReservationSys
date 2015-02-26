483be38596e934ad0cf4b3e0b291971ef32898bf
rhysinator19 <the_rock_2462@hotmail.com>
2015-02-25 23:59:31 -0500

    Signed-off-by: rhysinator19 <the_rock_2462@hotmail.com>

--- addRestaurant.php
+++ addRestaurant.php
@@ -11,7 +11,11 @@
 <html>
 
 	<head>
-		<title>Add Restaurant</title>
+		<title>
+			Add Restaurant
+		</title>
+		
+		<link rel="stylesheet" type="text/css" href="addRestaurant.css">
 	</head>
 	
 	<body>
@@ -23,11 +27,17 @@
 				?>
 			</div>
 			
-			<div id="content">
+			<div id="pageName">
+				<br>
+				<h2 class="white">Add a Restaurant</h3>
+			</div>
 			
+			<div id="content">
+				
 				<div id="description">
 				
 					<div id="restaurantName">
+						<br><br><br>
 						<h4>Restaurant Name:</h4>
 						<form>
 							<input type="text" name="restaurantName" size=35 maxlength=35>
@@ -38,23 +48,25 @@
 					<div id="features">
 						<h4>Features:</h4>
 						<form>
-							African:<input type="radio" name="African"> &nbsp
-							Alcohol Menu:<input type="radio" name="Alcohol Menu"> &nbsp
-							American:<input type="radio" name="American"> &nbsp
-							Buffet:<input type="radio" name="Buffet"> &nbsp
-							Casual Dining:<input type="radio" name="Casual Dining"> &nbsp <br>
-							Chinese:<input type="radio" name="Chinese"> &nbsp
-							Coffeehouse:<input type="radio" name="Coffeehouse"> &nbsp
-							Fast Food:<input type="radio" name="Fast Food"> &nbsp
-							Fine Dining:<input type="radio" name="Fine Dining"> &nbsp
-							Indian:<input type="radio" name="Indian"> &nbsp <br>
-							Irish:<input type="radio" name="Irish"> &nbsp
-							Italian:<input type="radio" name="Italian"> &nbsp 
-							Kid Friendly:<input type="radio" name="kidFriendly"> &nbsp
-							Korean:<input type="radio" name="Korean"> &nbsp
-							Pub:<input type="radio" name="Pub"> &nbsp <br>
-							Tabletop Cooking:<input type="radio" name="Tabletop Cooking"> &nbsp
-							Vegan:<input type="radio" name="Vegan"> &nbsp
+							African <input type="radio" name="african"></input> &nbsp
+							Alcohol Menu <input type="radio" name="alcoholMenu"></input> &nbsp
+							American <input type="radio" name="american"></input> &nbsp
+							Buffet <input type="radio" name="buffet"></input> &nbsp
+							Casual Dining <input type="radio" name="casualDining"></input> &nbsp <br>
+							Chinese <input type="radio" name="chinese"></input> &nbsp
+							Coffeehouse <input type="radio" name="coffeehouse"></input> &nbsp
+							Fast Food <input type="radio" name="fastFood"></input> &nbsp
+							Fine Dining <input type="radio" name="fineDining"></input> &nbsp
+							French <input type="radio" name="french"></input> &nbsp <br>
+							Indian <input type="radio" name="indian"></input> &nbsp 
+							Irish <input type="radio" name="irish"></input> &nbsp
+							Italian <input type="radio" name="italian"></input> &nbsp 
+							Japanese <input type="radio" name="japenese"></input> &nbsp
+							Kid Friendly <input type="radio" name="kidFriendly"> &nbsp <br>
+							Korean <input type="radio" name="korean"></input> &nbsp
+							Pub <input type="radio" name="pub"></input>&nbsp 
+							Tabletop Cooking <input type="radio" name="tabletopCooking"></input> &nbsp
+							Vegan <input type="radio" name="vegan"></input> &nbsp
 						</form>
 						<br>
 					</div>
@@ -62,7 +74,7 @@
 					<div id="priceRange">
 						<h4>Price Range:</h4>
 						<form>
-							<input type="text" name="priceRange" size=25 maxlength=25>
+							<input type="text" name="priceRange" size=25 maxlength=25></input>
 						</form>
 						<br>
 					</div>
@@ -70,39 +82,40 @@
 					<div id="hours">
 						<h4>Hours:</h4>
 						<form>
-							Sunday: &nbsp <input type="text" name="sundayStart" size=5 maxlength=4> &nbsp
-							to &nbsp <input type="text" name="sundayEnd" size=5 maxlength=4>
+							Sunday: &nbsp <input type="text" name="sundayStart" size=5 maxlength=4></input> &nbsp
+							to &nbsp <input type="text" name="sundayEnd" size=5 maxlength=4></input>
 						</form>
 						
 						<form>
-							Monday: &nbsp <input type="text" name="mondayStart" size=5 maxlength=4> &nbsp
-							to &nbsp <input type="text" name="mondayEnd" size=5 maxlength=4>
+							Monday: &nbsp <input type="text" name="mondayStart" size=5 maxlength=4></input> &nbsp
+							to &nbsp <input type="text" name="mondayEnd" size=5 maxlength=4></input>
 						</form>
 						
 						<form>
-							Tuesday: &nbsp <input type="text" name="tuesdayStart" size=5 maxlength=4> &nbsp
-							to &nbsp <input type="text" name="tuesdayEnd" size=5 maxlength=4>
+							Tuesday: &nbsp <input type="text" name="tuesdayStart" size=5 maxlength=4></input> &nbsp
+							to &nbsp <input type="text" name="tuesdayEnd" size=5 maxlength=4></input>
 						</form>
 						
 						<form>
-							Wednesday: &nbsp <input type="text" name="wednesdayStart" size=5 maxlength=4> &nbsp
-							to &nbsp <input type="text" name="wednesdayEnd" size=5 maxlength=4>
+							Wednesday: &nbsp <input type="text" name="wednesdayStart" size=5 maxlength=4></input> &nbsp
+							to &nbsp <input type="text" name="wednesdayEnd" size=5 maxlength=4></input>
 						</form>
 						
 						<form>
-							Thursday: &nbsp <input type="text" name="thursdayStart" size=5 maxlength=4> &nbsp
-							to &nbsp <input type="text" name="thursdayEnd" size=5 maxlength=4>
+							Thursday: &nbsp <input type="text" name="thursdayStart" size=5 maxlength=4></input> &nbsp
+							to &nbsp <input type="text" name="thursdayEnd" size=5 maxlength=4></input>
 						</form>
 						
 						<form>
-							Friday: &nbsp <input type="text" name="fridayStart" size=5 maxlength=4> &nbsp
-							to &nbsp <input type="text" name="fridayEnd" size=5 maxlength=4>
+							Friday: &nbsp <input type="text" name="fridayStart" size=5 maxlength=4></input> &nbsp
+							to &nbsp <input type="text" name="fridayEnd" size=5 maxlength=4></input>
 						</form>
 						
 						<form>
-							Saturday: &nbsp <input type="text" name="saturdayStart" size=5 maxlength=4> &nbsp
-							to &nbsp <input type="text" name="saturdayEnd" size=5 maxlength=4>
+							Saturday: &nbsp <input type="text" name="saturdayStart" size=5 maxlength=4></input> &nbsp
+							to &nbsp <input type="text" name="saturdayEnd" size=5 maxlength=4></input>
 						</form>
+						<br>
 					</div>
 					
 				</div>
@@ -112,7 +125,7 @@
 				</div>
 				
 				<div id="menu">
-				
+					
 				</div>
 				
 				<div id="rate">
@@ -120,17 +133,28 @@
 				</div>
 				
 				<div id="about">
-				
+					<h4>About:</h4>
+					<form>
+						<textarea cols="65" rows="8" name="about"></textarea>
+					</form>
+					<br>
 				</div>
 				
 				<div id="website">
-				
+					<h4>Website:</h4>
+					<form>
+						<input type="text" name="website" size=50 maxlength=100></input>
+					</form>
+					<br>
 				</div>
 				
 			</div>
 			
 			<div id="mainButtons">
-			
+				<br>
+				<form>
+					<input type="submit" value="Submit"></input>
+				</form>
 			</div>
 			
 			<div id="footer">
