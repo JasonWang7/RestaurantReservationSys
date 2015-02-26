3b8bdc96c667f18139dd640ac5088c8048328fd1
Vince <vtieu7@gmail.com>
2015-02-25 19:46:05 -0500

    views added

---
+++ modify.php
@@ -0,0 +1,14 @@
+<?php include("include/header.php"); ?>
+<div class="row">
+  <div class="col-12">  
+    <td>email :</td><td><input name="email" type="text" size"30"></input></td>
+    </tr>
+    <tr>
+    <td>password :</td><td><input name="pass1" type="password" size"30"></input></td>
+    </tr>
+    <tr>
+    <td>retype password :</td><td><input name="pass2" type="password" size"30"></input></td>
+    <input type="submit" value="Save Changes"></input> <input type="submit" value="Delete Account"></input>
+  </div>
+</div>
+<?php include("include/footer.php"); ?>
---
+++ verify.php
@@ -0,0 +1,11 @@
+<?php include("include/header.php"); ?>
+<div class="row">
+	<div class="col-12">	
+		<div class="jumbotron">
+		  <h1>Your account has been successfully verified.</h1>
+		  <p>Please proceed by</p>
+		  <p><a class="btn btn-primary btn-lg">Sign In</a></p>
+		</div>
+	</div>
+</div>
+<?php include("include/footer.php"); ?>
