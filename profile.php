3a5233b50f91a9573924d132083e563dc8d7c8f0
Vince <vtieu7@gmail.com>
2015-02-28 16:36:33 -0500

    Profile page

---
+++ profile.php
@@ -0,0 +1,29 @@
+<?php include("include/header.php"); ?>
+<div class="row">
+  <div class="col-12">  
+    <div class="jumbotron">
+      <div class="row">
+        <div class="col-md-3">
+          PHOTO HERE
+        </div>
+        <div class="col-md-9">
+                <h2>Name of Whatever</h2>
+                <h3>Phone: 519-412-3242 / Rating: 5 out of 5 (100)</h3>
+                <div class="row">
+                  <div class="btn-group btn-group-justified">
+                    <a href="#" class="btn btn-primary">Popular Restaurants</a>
+                    <a href="#" class="btn btn-primary">New Restaurants</a>
+                    <a href="#" class="btn btn-primary">Events</a>
+                    <a href="#" class="btn btn-primary">Reviews</a>
+                  </div>
+                </div>
+        </div>
+      </div>
+
+      <h1>Your account has been verified.</h1>
+      <p>Please proceed by signing in.</p>
+      <p><a class="btn btn-primary btn-lg">Sign In</a></p>
+    </div>
+  </div>
+</div>
+<?php include("include/footer.php"); ?>
--- verify.php
+++ verify.php
@@ -1,11 +1,11 @@
 <?php include("include/header.php"); ?>
 <div class="row">
-	<div class="col-12">	
-		<div class="jumbotron">
-		  <h1>Your account has been verified.</h1>
-		  <p>Please proceed by signing in.</p>
-		  <p><a class="btn btn-primary btn-lg">Sign In</a></p>
-		</div>
-	</div>
+  <div class="col-12">  
+    <div class="jumbotron">
+      <h1>Your account has been verified.</h1>
+      <p>Please proceed by signing in.</p>
+      <p><a class="btn btn-primary btn-lg">Sign In</a></p>
+    </div>
+  </div>
 </div>
 <?php include("include/footer.php"); ?>
