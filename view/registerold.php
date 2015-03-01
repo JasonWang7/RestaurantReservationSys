fab25d1804501af307d795fe814a855d81ca57be
Vince <vtieu7@gmail.com>
2015-03-01 15:28:17 -0500

    index page works

---
+++ view/registerold.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>register</title>
+</head>
+<body bgcolor="black" style="color:white;">
+<FORM ACTION="controller/usercontroller.php" METHOD=post>
+<h1>welcome to the registration page</h1>
+please input the registration details to create an account here<br>
+<table border="2">
+<tr>
+<td>User Name :</td><td><input name="username" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>email :</td><td><input name="email" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>password :</td><td><input name="pass1" type="password" size"30"></input></td>
+</tr>
+<tr>
+<td>retype password :</td><td><input name="pass2" type="password" size"30"></input></td>
+</tr>
+<tr>
+<td>first name :</td><td><input name="firstname" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>last name :</td><td><input name="lastname" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>phone :</td><td><input name="phone" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>address :</td><td><input name="address" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>city:</td><td><input name="city" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>postcode:</td><td><input name="postcode" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>rrole :</td><td><input name="role" type="text" size"30"></input></td>
+</tr>
+<tr>
+<td>likes :</td><td><input name="likes" type="text" size"30"></input></td>
+</tr>
+</table>
+<input type="submit" value="register me!"></input>
+</FORM>
+</body>
+</html>
---
+++ view/success.php
@@ -0,0 +1,1 @@
+<?php echo 'Account created.' ?>
