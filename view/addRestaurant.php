d45db262e551d76868d12367a302ac0643425308
rhysinator19 <the_rock_2462@hotmail.com>
2015-03-01 03:58:48 -0500

    Improved aesthetics of "Add Restaurant"

--- view/addRestaurant.php
+++ view/addRestaurant.php
@@ -13,7 +13,7 @@
 	<head>
 		<title>Add Restaurant</title>
 		
-		<link rel="stylesheet" type="text/css" href="css/addRestaurant.css">
+		<link rel="stylesheet" type="text/css" href="addRestaurant.css">
 	</head>
 	
 	<body>
@@ -28,58 +28,77 @@
 			
 			<!--title-->
 			<div id="pageName">
-				<h2 class="white">Add a Restaurant</h3>
+				<h2 class="white">Add a Restaurant</h2>
 			</div>
 			
 			<!--main content of page-->
 			<div id="content">
-				<form action="addRestaurantHandler.php" method="post">
+				<form action="controller/addRestaurantController.php" method="post">
 				
 					<div id="instructions">
-						<p>***Fill out information pertaining to each restaurant category then click "Submit" when finished</p>
+						<div id="instructionsText">
+							<p>*** Fill out information pertaining to each restaurant category then 
+							click "Submit" *** Optional categories are indicated as '(O)' and may be ignored</p>
+						</div>
 					</div>
 				
 					<!--all components of page where user enters data-->
 					<div id="input">
-					
+						
 						<div id="restaurantName">
-							<h4 class="blue">Restaurant Name:</h4>
-								<input type="text" name="restaurantName" size=35 maxlength=35>
+							<div id="restaurantNameText">
+								<h4 class="white">Restaurant Name</h4>
+							</div>
+							<input type="text" name="restaurantName" size=35 maxlength=35>
+						</div>
+						
+						<div id="address">
+							<div id="addressText">
+								<h4 class="white">Address</h4>
+							</div>
+							<input type="text" name="address" size=35 maxlength=35>
 						</div>
 					
-						<!--features of restaurant separated into radiobuttons-->
+						<!--features of restaurant separated into radio buttons-->
 						<div id="features">
-							<h4 class="blue">Features:</h4>
-								African <input type="radio" name="african"></input> &nbsp
-								Alcohol Menu <input type="radio" name="alcoholMenu"></input> &nbsp
-								American <input type="radio" name="american"></input> &nbsp
-								Buffet <input type="radio" name="buffet"></input> &nbsp
-								Casual Dining <input type="radio" name="casualDining"></input> &nbsp <br>
-								Chinese <input type="radio" name="chinese"></input> &nbsp
-								Coffeehouse <input type="radio" name="coffeehouse"></input> &nbsp
-								Fast Food <input type="radio" name="fastFood"></input> &nbsp
-								Fine Dining <input type="radio" name="fineDining"></input> &nbsp
-								French <input type="radio" name="french"></input> &nbsp <br>
-								Indian <input type="radio" name="indian"></input> &nbsp 
-								Irish <input type="radio" name="irish"></input> &nbsp
-								Italian <input type="radio" name="italian"></input> &nbsp 
-								Japanese <input type="radio" name="japanese"></input> &nbsp
-								Kid Friendly <input type="radio" name="kidFriendly"> &nbsp <br>
-								Korean <input type="radio" name="korean"></input> &nbsp
-								Pub <input type="radio" name="pub"></input>&nbsp 
-								Tabletop Cooking <input type="radio" name="tabletopCooking"></input> &nbsp
-								Vegan <input type="radio" name="vegan"></input> &nbsp
+							<div id="featuresText">
+								<h4 class="white">Features</h4>
+							</div>
+							
+							African <input type="radio" name="african"></input> &nbsp
+							Alcohol Menu <input type="radio" name="alcoholMenu"></input> &nbsp
+							American <input type="radio" name="american"></input> &nbsp
+							Buffet <input type="radio" name="buffet"></input> &nbsp
+							Casual Dining <input type="radio" name="casualDining"></input> &nbsp <br>
+							Chinese <input type="radio" name="chinese"></input> &nbsp
+							Coffeehouse <input type="radio" name="coffeehouse"></input> &nbsp
+							Fast Food <input type="radio" name="fastFood"></input> &nbsp
+							Fine Dining <input type="radio" name="fineDining"></input> &nbsp
+							French <input type="radio" name="french"></input> &nbsp <br>
+							Indian <input type="radio" name="indian"></input> &nbsp 
+							Irish <input type="radio" name="irish"></input> &nbsp
+							Italian <input type="radio" name="italian"></input> &nbsp 
+							Japanese <input type="radio" name="japanese"></input> &nbsp
+							Kid Friendly <input type="radio" name="kidFriendly"> &nbsp <br>
+							Korean <input type="radio" name="korean"></input> &nbsp
+							Pub <input type="radio" name="pub"></input>&nbsp 
+							Tabletop Cooking <input type="radio" name="tabletopCooking"></input> &nbsp
+							Vegan <input type="radio" name="vegan"></input> &nbsp
 						</div>
 					
 						<!--approximate price range of dining at restaurant-->
 						<div id="priceRange">
-							<h4 class="blue">Price Range:</h4>
+							<div id="priceRangeText">
+								<h4 class="white">Price Range</h4>
+							</div>
 							<input type="text" name="priceRange" size=25 maxlength=25></input>
 						</div>
 					
 						<!--hours corresponding to each day of the week-->
 						<div id="hours">
-							<h4 class="blue">Hours:</h4>
+							<div id="hoursText">
+								<h4 class="white">Hours</h4>
+							</div>
 						
 							Sunday: &nbsp <input type="text" name="sundayStart" size=5 maxlength=4></input> &nbsp
 							to &nbsp <input type="text" name="sundayEnd" size=5 maxlength=4></input>
@@ -117,13 +136,17 @@
 				
 						<!--Basic description of the restaurant-->
 						<div id="about">
-							<h4 class="blue">About:</h4>
+							<div id="aboutText">
+								<h4 class="white">About (O)</h4>
+							</div>
 							<textarea cols="65" rows="8" name="about"></textarea>
 						</div>
 				
 						<!--link to restaurant website (if owner currently has one)-->
 						<div id="website">
-							<h4 class="blue">Website:</h4>
+							<div id="websiteText">
+								<h4 class="white">Website (O)</h4>
+							</div>
 							<input type="text" name="website" size=50 maxlength=100></input>
 						</div>		
 					</div>
