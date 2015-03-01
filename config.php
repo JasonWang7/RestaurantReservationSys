0f39b22acfa6081ccd059dfadaa01debcd0798f7
rhysinator19 <the_rock_2462@hotmail.com>
2015-02-28 21:48:56 -0500

    Added setter functions to restaurant model view

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
 $configs = array();
 $configs['db_host_rrsframe'] = 'localhost';
 $configs['db_user_rrsframe'] = 'root';
-$configs['db_pass_rrsframe'] = 'jasonwang';
+$configs['db_pass_rrsframe'] = 'roadkill182';
 $configs['db_name_rrsframe'] = 'rss_reservation';
 
 ?>
--- models/restaurant.php
+++ models/restaurant.php
@@ -11,7 +11,8 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 require_once($root.'/RRS/authentication.class.php');
 require_once($root.'/RRS/database.class.php');
 
-class restaurant{
+class restaurant
+{
 	//text field variables
 	private $restaurantName;
 	private $priceRange;
@@ -52,6 +53,27 @@ class restaurant{
 	private $pub;
 	private $tableTopCooking;
 	private $vegan;
+	
+	
+	/**
+	* retrieve all the information about a given restaurant
+	* @param email
+	* @return restaurant obj
+	*/
+	function selectRestaurantInfo($email)
+	{
+		
+	}
+	
+	
+	/**
+	* insert new information corresponding to a given restaurant into the database system
+	* @return true on success, false otherwise
+	*/
+	function insertRestaurantInfo()
+	{
+		
+	}
 	
 	//getter functions
 	function getRestaurantName()
@@ -129,8 +151,291 @@ class restaurant{
 		return $this->saturdayStart;
 	}
 	
-	function saturdayEnd()
+	function getSaturdayEnd()
 	{
 		return $this->saturdayEnd;
+	}
+	
+	function getAfrican()
+	{
+		return $this->african;
+	}
+	
+	function getAlcoholMenu()
+	{
+		return $this->alcoholMenu;
+	}
+	
+	function getAmerican()
+	{
+		return $this->american;
+	}
+	
+	function getBuffet()
+	{
+		return $this->buffet;
+	}
+	
+	function getCasualDining()
+	{
+		return $this->casualDining;
+	}
+	
+	function getChinese()
+	{
+		return $this->chinese;
+	}
+	
+	function getCoffeehouse()
+	{
+		return $this->coffeehouse;
+	}
+	
+	function getFastFood()
+	{
+		return $this->fastFood;
+	}
+	
+	function getFineDining()
+	{
+		return $this->fineDining;
+	}
+	
+	function getFrench()
+	{
+		return $this->french;
+	}
+	
+	function getIndian()
+	{
+		return $this->indian;
+	}
+	
+	function getIrish()
+	{
+		return $this->irish;
+	}
+	
+	function getItalian()
+	{
+		return $this->italian;
+	}
+	
+	function getJapanese()
+	{
+		return $this->japanese;
+	}
+	
+	function getKidFriendly()
+	{
+		return $this->kidFriendly;
+	}
+	
+	function getKorean()
+	{
+		return $this->korean;
+	}
+	
+	function getPub()
+	{
+		return $this->pub;
+	}
+	
+	function getTableTopCooking()
+	{
+		return $this->tableTopCooking;
+	}
+	
+	function getVegan()
+	{
+		return $this->vegan;
+	}
+	
+	
+	//setters
+	
+	function setRestaurantName($param)
+	{
+		$this->restaurantName = $param;	
+	}
+	
+	function setPriceRange($param)
+	{
+		$this->priceRange = $param;	
+	}
+	
+	function setAbout($param)
+	{
+		$this->about = $param;	
+	}
+	
+	function setWebsite($param)
+	{
+		$this->website = $param;	
+	}
+	
+	function setSundayStart($param)
+	{
+		$this-> = $param;	
+	}
+	
+	function setSundayEnd($param)
+	{
+		$this-> = $param;	
+	}
+	
+	function setMondayStart($param)
+	{
+		$this->mondayStart = $param;	
+	}
+	
+	function setMondayEnd($param)
+	{
+		$this->mondayEnd = $param;	
+	}
+	
+	function setTuesdayStart($param)
+	{
+		$this->tuesdayStart = $param;	
+	}
+	
+	function setTuesdayEnd($param)
+	{
+		$this->tuesdayEnd = $param;	
+	}
+	
+	function setWednesdayStart($param)
+	{
+		$this->setWednesdayStart = $param;	
+	}
+	
+	function setWednesdayEnd($param)
+	{
+		$this->wednesdayEnd = $param;	
+	}
+	
+	function setThursdayStart($param)
+	{
+		$this->thursdayStart = $param;	
+	}
+	
+	function setThursdayEnd($param)
+	{
+		$this->thursdayEnd = $param;	
+	}
+	
+	function setFridayStart($param)
+	{
+		$this->fridayStart = $param;	
+	}
+	
+	function setFridayEnd($param)
+	{
+		$this->fridayEnd = $param;	
+	}
+	
+	function setSaturdayStart($param)
+	{
+		$this->saturdayStart = $param;	
+	}
+	
+	function setSaturdayEnd($param)
+	{
+		$this->saturdayEnd = $param;	
+	}
+	
+	function setAfrican($param)
+	{
+		$this->african = $param;	
+	}
+	
+	function setAlcoholMenu($param)
+	{
+		$this->alcoholMenu = $param;	
+	}
+	
+	function setAmerican($param)
+	{
+		$this->american = $param;	
+	}
+	
+	function setBuffet($param)
+	{
+		$this->buffet = $param;	
+	}
+	
+	function setCasualDining($param)
+	{
+		$this->casualDining = $param;	
+	}
+	
+	function setChinese($param)
+	{
+		$this->chinese = $param;	
+	}
+	
+	function setCoffeeHouse($param)
+	{
+		$this->coffeehouse = $param;	
+	}
+	
+	function setFastFood($param)
+	{
+		$this->fastFood = $param;	
+	}
+	
+	function setFineDining($param)
+	{
+		$this->fineDining = $param;	
+	}
+	
+	function setFrench($param)
+	{
+		$this->french = $param;	
+	}
+	
+	function setIndian($param)
+	{
+		$this->indian = $param;	
+	}
+	
+	function setIrish($param)
+	{
+		$this->irish = $param;	
+	}
+	
+	function setItalian($param)
+	{
+		$this->italian = $param;	
+	}
+	
+	function setJapanese($param)
+	{
+		$this->japanese = $param;	
+	}
+	
+	function setKidFriendly($param)
+	{
+		$this->kidFriendly = $param;	
+	}
+	
+	function setKorean($param)
+	{
+		$this->korean = $param;	
+	}
+	
+	function setPub($param)
+	{
+		$this->pub = $param;	
+	}
+	
+	function setTableTopCooking($param)
+	{
+		$this->tableTopCooking = $param;	
+	}
+	
+	function setVegan($param)
+	{
+		$this->vegan = $param;	
 	}
 }
