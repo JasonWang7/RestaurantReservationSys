ae6cbec7db73d3a2ae311fc0320cbec7e09cc3b8
Vince <vtieu7@gmail.com>
2015-03-01 15:37:24 -0500

    Index is fully working now

--- index.php
+++ index.php
@@ -4,48 +4,49 @@
 <?php 
 $root = $_SERVER['DOCUMENT_ROOT'].'/RRS/';
 include($root."view/include/header.php"); 
-
+include($root ."util/database.class.php");
 ?>
+
 <div class="row">
-	<div class="col-md-12">	
-		<form style="font-size:28px" class="navbar-form navbar-left" role="search">
-						Find a Resturant: 
-		        <div class="form-group">
-		          <input type="text" class="form-control" placeholder="Search">
-		        </div>
-		        <button type="submit" class="btn btn-default">Submit</button>
-		</form>
-		<div style="font-size:28px" class="navbar-form navbar-right" role="search">
-			Or if you can't find one.	<a href="addrestaurant">Add a Resturant</a>
-		</div>
-	</div>
+  <div class="col-md-12"> 
+    <form style="font-size:28px" class="navbar-form navbar-left" role="search">
+            Find a Resturant: 
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+    <div style="font-size:28px" class="navbar-form navbar-right" role="search">
+      Or if you can't find one. <a href="addrestaurant">Add a Resturant</a>
+    </div>
+  </div>
 </div>
 <p><p>
 <div class="row">
-	<div class="col-md-12">	
-		<div class="jumbotron">
-		<div class="row">
-		  <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
-		  <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
-		  <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
-		  <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
-		  <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
-		  <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; width:50px; height:180px;"><a href="#">></a></div>
-		</div>
-		</div>
-	</div>
+  <div class="col-md-12"> 
+    <div class="jumbotron">
+    <div class="row">
+      <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
+      <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
+      <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
+      <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
+      <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; height:180px;">"This dining place is good...." by Test Guy<p>on <a href="#">Bob Diner</a></div>
+      <div class="well col-md-2" style="font-size:18px; background-color:#eee; margin:8px; text-align:middle; width:50px; height:180px;"><a href="#">></a></div>
+    </div>
+    </div>
+  </div>
 </div>
 <div class="row">
-	<div class="btn-group btn-group-justified">
-	  <a href="#" class="btn btn-primary">Popular Restaurants</a>
-	  <a href="#" class="btn btn-primary">New Restaurants</a>
-	  <a href="#" class="btn btn-primary">Events</a>
-	  <a href="#" class="btn btn-primary">Reviews</a>
-	</div>
+  <div class="btn-group btn-group-justified">
+    <a href="#" class="btn btn-primary">Popular Restaurants</a>
+    <a href="#" class="btn btn-primary">New Restaurants</a>
+    <a href="#" class="btn btn-primary">Events</a>
+    <a href="#" class="btn btn-primary">Reviews</a>
+  </div>
 </div>
 <hr>
 <div class="row">
-	<table class="table table-striped table-hover ">
+  <table class="table table-striped table-hover ">
   <thead>
     <tr>
       <th>#</th>
@@ -55,48 +56,30 @@ include($root."view/include/header.php");
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>1</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr>
-      <td>4</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr>
-      <td>5</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr>
-      <td>6</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr>
-      <td>7</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
+    <?php
+      
+      $dbconn = mysqldatabaserrs::connectdb();
+    $query = "select * from restaurant";
+    try {
+
+    $stmt = $dbconn->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+
+    $stmt->execute();
+    
+    while($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT))
+    {
+      $data = '<tr>' . '<td>' . $row[0] . '</td><td><a href="profile?id=' . $row[0] . '">'.$row[3].'</td><td>' . $row[6] . "</td><td>" . $row[7] . '</td></tr>';
+      echo $data . '</a>';
+    }
+    $stmt = null;
+
+    }
+    catch (PDOException $e) {
+      print $e->getMessage();
+    }
+
+    mysqldatabaserrs::closeconnction($dbconn);
+    ?>
   </tbody>
 </table> 
 </div>
