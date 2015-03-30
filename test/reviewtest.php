a899cc74482f19db029f61ff272565ff8929df27
jason <jason>
2015-03-30 17:46:55 -0400

    -add comment

--- test/reviewtest.php
+++ test/reviewtest.php
@@ -4,6 +4,7 @@ $root = $_SERVER['DOCUMENT_ROOT'].'/RRS/';
 require_once($root.'util/authentication.class.php');
 require_once($root.'util/database.class.php');
 require_once($root.'model/review.php');
+/****how to add review****/
 date_default_timezone_set('America/Toronto');
 $reviewobj = new review;
 $reviewobj->setUserId(65);
@@ -18,14 +19,22 @@ $reviewobj->setVotes(0);
 $reviewobj->setReviewTime(date("Y-m-d H:i:s"));
 $reviewobj->insertReview();
 echo "inserted new review with review id:".$reviewobj->getReviewId();
+
+
+/*****how to update review*******/
 $reviewobj->setComment("Test Update comment");
 echo '<pre>'.print_r($reviewobj, true).'</pre>'; 
 $reviewobj->updateReview($reviewobj);
+
+/*****delete review*****/
 $reviewobj->deleteReview(2);
+
+/****get list review that user made*****/
 $x = $reviewobj->listReviewById(65,5);
 //echo $x;
 echo '<pre>'.print_r($x, true).'</pre>'; 
 
+/*******get newest review order by time pagnated by offset number*****/
 echo 'get newest reviews';
 $z = $reviewobj->listReview(0);
 echo '<pre>'.print_r($z, true).'</pre>'; 
