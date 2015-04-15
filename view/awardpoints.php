9f28236b1560ca99dbcc4a612d109d86518ad4cc
Vince <vtieu7@gmail.com>
2015-04-15 18:57:05 -0400

    Added success pages for event reservations and awardpoints

---
+++ view/awardpoints.php
@@ -0,0 +1,12 @@
+<?php 
+/****Vince ****/
+include("include/header.php"); ?>
+<div class="row">
+  <div class="col-12">  
+    <div class="jumbotron">
+      <h2>You've been awarded points</h2>
+      <p>Your account has just been credited X amount of points.</p>
+    </div>
+  </div>
+</div>
+<?php include("include/footer.php"); ?>
