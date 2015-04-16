0df70b821dadc87f34a21f437d84ef57f517d570
jason <jason>
2015-04-16 12:45:28 -0400

    -Add event and upload event image working, List Event

--- view/profileeventtab.php
+++ view/profileeventtab.php
@@ -3,9 +3,9 @@
 author: Vince, jason wang
 */
 include_once($root.'model/event.php');
-$reviewobj = new review;
+$eventobj = new event;
 $userid = $_SESSION['sess_user_id'];
-$reviewlist = $reviewobj->listReviewById($userid,0);
+$eventlist = $eventobj->retriveListEventByrestaurantId($_GET["id"],0);
 $renderbody="";
 $tablehead="";
 $tablebody="";
@@ -14,225 +14,27 @@ $tableend="";
 //echo $userid;
 //echo '<pre>'.print_r($reviewlist, true).'</pre>'; 
 if(count($reviewlist)>0){
-	$tablehead =' <div class="row" style="padding:10px;">';
+	$tablehead ='';
     $tableend = '';  
-    foreach($reviewlist as $r){
-    	$tablebody = $tablebody.'<tr>' . '<td>' .$r->getReviewId() . '</td><td><a href="profile?id=' . $r->getRestaurantId(). '">'.$r->getRestaurantName().'</td><td>' .$r->getComment(). "</td><td>" . $r->getOverallExp() .
-                '</td><td>'.$r->getReviewTime().'</td><td><a class="btn btn-default" href="#"  data-toggle="modal" data-target="#reviewmodal'.$r->getReviewId().'">View</a></td>'.'<td><a class="btn btn-primary" href="deletereview?id='.$r->getReviewId().'" >Delete</a></td>'.'</tr>'.'</a>'.
-                '<div class="modal fade" id="reviewmodal'.$r->getReviewId().'" tabindex="-1" role="dialog" aria-labelledby="reviewmodal'.$r->getReviewId().'" aria-hidden="true">
-				  <div class="modal-dialog">
-				    <div class="modal-content">
-				      <form id="review" name="review" ACTION="modifyreview" METHOD=post>
-				      <div class="modal-header">
-				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				        <h4 class="modal-title" id="label">Review for '.$r->getRestaurantName().'</h4>
-				      </div>
-
-				      <div class="modal-body">
-				        
-				        <div class="row">
-				          <div class="col-md-12">
-				            <h3>Service:       
-				              <select class="form-control" id="servicerating" name="servicerating" >';
-							  if($r->getServiceRating()==1){
-							  		$ops = '<option selected="selected">1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getServiceRating()==2){
-							  		$ops = '<option>1</option>
-							  		<option selected="selected">2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getServiceRating()==3){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option selected="selected">3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getServiceRating()==4){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option selected="selected">4</option>
-					              	<option>5</option>';
-
-							  }
-							  else if($r->getServiceRating()==5){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option selected="selected">5</option>';
-							  }
-				              $tablebody=$tablebody.$ops;
-				              $ops='';
-				             
-				            $tablebody=$tablebody.'</select>
-				        </h3>
-				          </div>
-				        </div>
-				        <div class="row">
-				          <div class="col-md-12">
-				            <h3>Food:               
-				            <select class="form-control" id="foodrating" name="foodrating" >';
-				            if($r->getFoodRating()==1){
-							  		$ops = '<option selected="selected">1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getFoodRating()==2){
-							  		$ops = '<option>1</option>
-							  		<option selected="selected">2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getFoodRating()==3){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option selected="selected">3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getFoodRating()==4){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option selected="selected">4</option>
-					              	<option>5</option>';
-
-							  }
-							  else if($r->getFoodRating()==5){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option selected="selected">5</option>';
-							  }
-				              $tablebody=$tablebody.$ops;
-				              $ops='';
-				              
-				            $tablebody=$tablebody.'</select></h3>
-				          </div>
-				        </div>
-				        <div class="row">
-				          <div class="col-md-12">
-				            <h3>Ambience:               
-				            <select class="form-control" id="ambiencerating" name="ambiencerating" >';
-				              if($r->getAmbienceRating()==1){
-							  		$ops = '<option selected="selected">1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getAmbienceRating()==2){
-							  		$ops = '<option>1</option>
-							  		<option selected="selected">2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getAmbienceRating()==3){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option selected="selected">3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getAmbienceRating()==4){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option selected="selected">4</option>
-					              	<option>5</option>';
-
-							  }
-							  else if($r->getAmbienceRating()==5){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option selected="selected">5</option>';
-							  }
-				              $tablebody=$tablebody.$ops;
-				              $ops='';
-				              
-				            $tablebody=$tablebody.'</select></h3>
-				          </div>
-				        </div>
-				        <div class="row">
-				          <div class="col-md-12">
-				            <h3>Overall Experience:               
-				            <select class="form-control" id="overallexp" name="overallexp" >';
-				              if($r->getOverallExp()==1){
-							  		$ops = '<option selected="selected">1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getOverallExp()==2){
-							  		$ops = '<option>1</option>
-							  		<option selected="selected">2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getOverallExp()==3){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option selected="selected">3</option>
-					              	<option>4</option>
-					              	<option>5</option>';
-							  }
-							  else if($r->getOverallExp()==4){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option selected="selected">4</option>
-					              	<option>5</option>';
-
-							  }
-							  else if($r->getOverallExp()==5){
-							  		$ops = '<option>1</option>
-							  		<option>2</option>
-					              	<option>3</option>
-					              	<option>4</option>
-					              	<option selected="selected">5</option>';
-							  }
-				              $tablebody=$tablebody.$ops;
-				              $ops='';
-				              
-				            $tablebody=$tablebody.'</select></h3>      
-				          </div>
-				        </div>
-				        <div class="row">
-				          <div class="col-md-12">
-				            <h3>Comments:</h3>
-				            <textarea id="comment" name="comment" style="overflow: hidden; word-wrap: break-word; resize: horizontal; width:100%; height: 100px;">'.$r->getComment().'</textarea>
-				            <input type="hidden" name="reviewid" value='.$r->getReviewId().' >
-				          </div>
-				        </div>
-				      
-				      </div>
-				      <div class="modal-footer">
-				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				        <button type="submit" class="btn btn-primary">Submit</button>
-				      </div>
-				     </form> 
-				    </div>
-				  </div>
-				</div>
-                ';
+    foreach($eventlist as $ev){
+    	$tablebody = $tablebody.'<div class="row" style="padding:10px;"><hr><div class="col-md-2" style="floating:right;">'.
+        '<p>'.'<img src="'.$ev->getEventPictureUrl().'" alt="Event Image" height="170" width="170">'.'</p>'.      
+      '</div>
+      <div class="col-md-2">
+        <p><h4>Event:</h4> '.$ev->getEventName().'</p>'.
+      '</div>
+      <div class="col-md-2">
+        <p><h4>Time:</h4>'.$ev->getEventTime().' - '.$ev->getEventEndTime().'</p>'.
+      '</div>
+      <div class="col-md-4">
+        <p><h4>Description:</h4> '.$ev->getEventDiscription().'</p>'.
+      '</div>
+      <div class="col-md-2">
+        <a class="btn btn-success" href="#">Book Event</a>
+       	<br />
+        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modifyeventmodal">Modify</a>
+        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modifyeventmodal">Delete</a>
+      </div></div>';
     }         
     $renderbody=$tablehead.$tablebody.$tableend;
     echo $renderbody;
